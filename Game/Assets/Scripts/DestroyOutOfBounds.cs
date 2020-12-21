82e619956a984a28fd1bc08b638357e223ea6e18
jessicattati <72874390+jessicattati@users.noreply.github.com>
2020-12-21 19:57:31 +0000

    adding scenes

---
+++ Game/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
